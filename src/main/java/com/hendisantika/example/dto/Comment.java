910761462cb7a5a494b261a5652fd4ab7e786351
Hendi Santika <hendisantika@gmail.com>
2018-09-15 05:55:34 +0700

    #9 - Add title in README.md file

--- src/main/java/com/hendisantika/example/dto/Comment.java
+++ src/main/java/com/hendisantika/example/dto/Comment.java
@@ -1,6 +1,7 @@
 package com.hendisantika.example.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-scheduler-example
@@ -14,41 +15,51 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Comment extends BaseDto {
 
-	Integer postId;
-	Integer id;
-	String name;
-	String email;
-	String body;
-	public Integer getPostId() {
-		return postId;
-	}
-	public void setPostId(Integer postId) {
-		this.postId = postId;
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getBody() {
-		return body;
-	}
-	public void setBody(String body) {
-		this.body = body;
-	}
-	
-	
+    Integer postId;
+    Integer id;
+    String name;
+    String email;
+    String body;
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+
 }
